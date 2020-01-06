31399f889638823a21e535e7d4c75e4981bc5cf8
Mingxun Wang <mwang87@gmail.com>
2020-01-06 12:11:04 -0800

    adding end point

--- test-production-integration/test_integration.py
+++ test-production-integration/test_integration.py
@@ -6,3 +6,9 @@ def test_production():
     url = f"https://{PRODUCTION_URL}/heartbeat"
     r = requests.get(url)
     r.raise_for_status()
+
+
+def test_gnps_library():
+    url = f"https://{PRODUCTION_URL}/gnpslibraryjson"
+    r = requests.get(url)
+    r.raise_for_status()
