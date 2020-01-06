adafcfbdab79422224ddd88146c6e7834e5c8a9f
Mingxun Wang <mwang87@gmail.com>
2020-01-06 11:57:23 -0800

    debugging code

--- gnps_tasks.py
+++ gnps_tasks.py
@@ -12,17 +12,17 @@ def generate_gnps_data():
     libraries_json = utils.load_GNPS()
 
     print("Got all Libraries")
-    
+
     with open("/output/gnpslibraries.json", "w") as output_file:
         output_file.write(json.dumps(libraries_json))
 
-    with open("/output/gnpslibraries_all_formated.json", "w") as output_file:
-        output_file.write(json.dumps(utils.gnps_filter_for_key(libraries_json, filterKeysOut=False)))
+    # with open("/output/gnpslibraries_all_formated.json", "w") as output_file:
+    #     output_file.write(json.dumps(utils.gnps_filter_for_key(libraries_json, filterKeysOut=False)))
 
-    with open("/output/gnpslibraries_withkeys_formated.json", "w") as output_json:
-        output_file.write(json.dumps(utils.gnps_filter_for_key(libraries_json, filterKeysOut=True)))
+    # with open("/output/gnpslibraries_withkeys_formated.json", "w") as output_json:
+    #     output_file.write(json.dumps(utils.gnps_filter_for_key(libraries_json, filterKeysOut=True)))
 
-    pd.DataFrame(utils.gnps_filter_for_key(libraries_json)).to_csv("/output/gnpslibraries_withkeys_formated.tsv", sep="\t", index=False)
+    # pd.DataFrame(utils.gnps_filter_for_key(libraries_json)).to_csv("/output/gnpslibraries_withkeys_formated.tsv", sep="\t", index=False)
 
 celery_instance.conf.beat_schedule = {
     "generate_gnps_data": {
